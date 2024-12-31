b53bdef52412308852cdfed5c1c06917a08f1c81
loteque <andrewandroid@gmail.com>
2024-12-30 17:12:53 -0800

    update helath_component, remove world.tscn

--- templates/HealthComponent.gd
+++ templates/HealthComponent.gd
@@ -4,7 +4,42 @@ class_name HealthComponent
 
 @export var health: float:
     set(f):
+        if f == health:
+            return
+        if healable == false and f > health:
+            return
+        if revivable == false and health == 0:
+            return
+        if invincible == true and f < health:
+            return
         health = f
         health_changed.emit(health)
 
+@export var max_health: float:
+    set(f):
+        health = f
+        max_health_changed.emit(max_health)
+
+@export var invincible: bool = false:
+    set(b):
+        invincible = b
+        invincible_changed.emit(invincible)
+
+@export var healable: bool = true:
+    set(b):
+        healable = b
+        healable_changed.emit(healable)
+
+@export var revivable: bool = false:
+    set(b):
+        revivable = b
+        revivable_changed.emit(revivable)
+
 signal health_changed(health)
+signal max_health_changed(max_health)
+signal invincible_changed(invincible)
+signal healable_changed(healable)
+signal revivable_changed(revivable)
+
+func set_health_to_max():
+    health = max_health
