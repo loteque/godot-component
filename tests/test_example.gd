0c5298a5603abd2bfa10bca06431a1f21f7bfaaf
loteque <andrewandroid@gmail.com>
2025-01-01 15:15:32 -0800

    update tests

--- tests/test_example.gd
+++ tests/test_example.gd
@@ -1,18 +1,25 @@
 @tool
 extends Test
 
-func _init() -> void:
+func init_module() -> void:
     assertions.append_array(get_assertions())
     module = null
 
 var identify := Assertion.new(
     func(i := false):
-        if self: i = true
-        return i
+    if self: i = true
+    return i
 )
 
 var inside_tree := Assertion.new(
     func(i := false):
-        if self.is_inside_tree(): i = true
-        return i
+    if self.is_inside_tree(): i = true
+    return i
+)
+
+var inside_tree_is_false := Assertion.new(
+    func():
+    return inside_tree.method.call()
+    ,
+    Assertion.Type.ASSERT_FALSE
 )
